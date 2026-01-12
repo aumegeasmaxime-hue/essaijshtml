7f46254a678ff9e8845359cedf827e9fb0a5426e
maxime <maxime.aumegeas@laplateforme.io>
2026-01-12 14:28:34 +0100

    modif

--- essai/script.js
+++ essai/script.js
@@ -5,26 +5,33 @@ document.addEventListener("keydown",function(event){
         switch (event.key){
             case "ArrowUp":
                 document.getElementById("joueur").style.top = (countTop +"px")
-                countTop--
-                console.log(countBottom)
+                countTop--;
+                console.log(countTop);
+                console.log(countLeft + " left");
                 break;  
             case "ArrowDown":
                 document.getElementById("joueur").style.top = (countTop +"px")
-                countTop++
-                console.log(countTop)
+                countTop++;
+                console.log(countTop);
+                console.log(countLeft + " left");
                 break;
             case "ArrowLeft":
                 document.getElementById("joueur").style.left = (countLeft +"px")
-                countLeft--
-                console.log(countRight)
+                countLeft--;
+                console.log(countLeft);
                 break;
             case "ArrowRight":
                 document.getElementById("joueur").style.left = (countLeft +"px")
-                countLeft++
-                console.log(countLeft)
+                countLeft++;
+                console.log(countLeft);
                 break;
         }
-        
+        if ((countTop == 21)&&((0<=countLeft)&&(countLeft<30))){
+            countTop = 22;
+        }
+        if ((countTop == 20)&&((0<=countLeft)&&(countLeft<30))){
+            countTop = 19;
+        }
 
 
 

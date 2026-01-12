eca286b96513e61d958e8cc99070462732d81293
maxime <maxime.aumegeas@laplateforme.io>
2026-01-12 15:18:46 +0100

    modif

--- jour2/job6/script.js
+++ jour2/job6/script.js
@@ -22,57 +22,24 @@ document.addEventListener("keydown",function(event){
             tabEntrees = [];
         }
     
-                      
-           
         
+/*  Methode 2
 
+const codesKonami = ["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft","ArrowRight","ArrowLeft","ArrowRight","b","a"];
+let codesUtilisateur = [];
 
+document.addEventListener("keydown" , event => {
+codesUtilisateur.push(event.key)
+console.log(codesUtilisateur)
+for (i = 0; i<codesUtilisateur.length; i++) {
+    if (codesKonami[i]!=codesUtilisateur[i]) {
+        console.log("Failed");
+        codesUtilisateur = [];
+    } else if (codesUtilisateur.length == codesKonami.length) {
+        console.log("Password OK")
+        codesUtilisateur = [];
+    }
+}});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //console.log(event.key)
-        console.log(tabEntrees)
-        //console.log(tabEntrees.length)
+*/
 })

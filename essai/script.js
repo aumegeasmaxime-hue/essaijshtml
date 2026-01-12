07cfb5d113318880610a96d1491198c97e99de04
maxime <maxime.aumegeas@laplateforme.io>
2026-01-12 11:21:09 +0100

    add exo complementaire

--- essai/script.js
+++ essai/script.js
@@ -1,19 +1,28 @@
-let count = 0;
+let countTop = 0;
+let countLeft = 0;
 document.addEventListener("keydown",function(event){
     console.log(event.key)
         switch (event.key){
             case "ArrowUp":
-                document.getElementById("joueur").style.left = "1px"
-
+                document.getElementById("joueur").style.top = (countTop +"px")
+                countTop--
+                console.log(countBottom)
+                break;  
             case "ArrowDown":
-                document.getElementById("joueur").style.marginTop = "101px"     
-
+                document.getElementById("joueur").style.top = (countTop +"px")
+                countTop++
+                console.log(countTop)
+                break;
             case "ArrowLeft":
-                document.getElementById("joueur").style.marginRight = "101px"
-               
+                document.getElementById("joueur").style.left = (countLeft +"px")
+                countLeft--
+                console.log(countRight)
+                break;
             case "ArrowRight":
-                document.getElementById("joueur").style.marginLeft = "101px"
-                
+                document.getElementById("joueur").style.left = (countLeft +"px")
+                countLeft++
+                console.log(countLeft)
+                break;
         }
         
 

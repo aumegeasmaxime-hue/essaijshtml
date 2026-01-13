ee2a19e08b43e659611a10f14c239b7014afa318
maxime <maxime.aumegeas@laplateforme.io>
2026-01-13 16:53:42 +0100

    modif

--- jour3/job3/script.js
+++ jour3/job3/script.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 let button = document.getElementById("button");
 button.addEventListener('click',function(){
     let champNom = document.getElementById("nom");    
@@ -17,6 +13,7 @@ let champType = document.getElementById("type");
 if ((id == null)&&(nom == null)){
     // requete par type
     
+    
 
 
 
@@ -30,4 +27,22 @@ else if ((nom == null)&&(type == null)){
     // requete par id
 
     
+}
+
+
+
+async function fetchArticle(){
+    try{
+        let response = await fetch("http://localhost:8080/all");
+        if (!response.ok){
+            throw new Error("error in fetch");            
+        }
+        let data = await response.json();
+            for (let article of data){
+            createArticle(article);
+        }
+    }
+    catch(error){
+        console.error(error);
+    }
 }
